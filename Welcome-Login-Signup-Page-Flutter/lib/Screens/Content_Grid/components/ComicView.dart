26eae684692132341f40f690ad9a19a41259d7b1
el17dtt <el17dtt@leeds.ac.uk>
2023-01-22 15:10:30 +0200

    Removing files files after adding .gitignore 2

---
+++ Welcome-Login-Signup-Page-Flutter/lib/Screens/Content_Grid/components/ComicView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ComicView extends StatelessWidget {
+  const ComicView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build of the view of the comic in the grid
+
+    throw UnimplementedError();
+  }
+}
